80f31e5b1eca3467294901f12aa92d305d97affb
ambhikishan <82771838+ambhikishan@users.noreply.github.com>
2023-11-18 10:54:42 +0530

    Add files via upload

--- my/watermark/watermark.java
+++ my/watermark/watermark.java
@@ -15,10 +15,25 @@ class watermark {
         g.setColor(Color.WHITE);
         int x = img.getWidth(), y = img.getHeight();
         g.setFont(new Font("October Twilight",1,x*5/100));
+        String file = f.getName();
 
+
+        int count=0;
+
+        for(int i = 0; i<file.length()-1; i++) {
+            if (file.charAt(i) == '.') {
+                count = i;
+            }
+        }
+        StringBuilder format = new StringBuilder();
+
+        for(int i =count+1;i<file.length();i++)
+            format.append(file.charAt(i));
+        String frm = format.toString();
         g.drawString(text,100,y-y*2/100);
-        ImageIO.write(img,"png",new File(f.getParent()+"/w-"+f.getName()));
-        ImageIO.write(img,"jpg",new File(f.getParent()+"/w-"+f.getName()));
+
+        ImageIO.write(img,frm,new File(f.getParent()+"/w-"+f.getName()));
+//        ImageIO.write(img,"jpg",new File(f.getParent()+"/w-"+f.getName()));
     }
 
 }
